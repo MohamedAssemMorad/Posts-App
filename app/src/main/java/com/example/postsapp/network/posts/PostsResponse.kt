11d7b93d3e99eda3bac744156268d0fc0405ed2f
Mohamed Assem Mourad <m.mourad@EGA.LAN>
2019-12-31 10:41:44 +0200

    reading data from API

--- app/src/main/java/com/example/postsapp/network/posts/PostsResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.postsapp.network.posts
-
-import com.google.gson.annotations.SerializedName
-
-data class PostsResponse(
-
-    @SerializedName("")
-    private val result: PostsResult? = null
-)
---
+++ app/src/main/java/com/example/postsapp/utils/FragmentUtil.kt
@@ -0,0 +1,4 @@
+package com.example.postsapp.utils
+
+object FragmentUtil {
+}
